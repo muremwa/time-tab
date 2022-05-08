d413393ba2e70cbdc4afee09cc8e2d787d58bc70
Muremwa <danmburu254@gmail.com>
2022-05-08 17:33:35 +0300

    fixes 12 - 24 hr system

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const months = [
     'January', 'February', 'March', 'April', 'May', 'June',
     'July', 'August', 'September', 'October', 'November', 'December'
 ];
-let fHate = 0;
+let fHate = 12;
 let soundOn = 1;
 
 // add 0 to numbers below ten: 9 -> 09, 12 -> 12, assumes all numbers are positive
@@ -23,47 +23,48 @@ const zeroPen = (num) => num < 10? `0${num.toString()}`: num.toString();
 * */
 function timeExtract () {
     // date
-    const date = new Date();
+    const date = new Date('2012-12-06 00:00:00');
     const day = days[date.getDay()];
     const month = months[date.getMonth()];
 
     // time
-    const hours = date.getHours();
+    let hours = date.getHours();
     const fHateMark = fHate ? hours > 11 ? ' PM' : ' AM' : '';
+    const tempfhate = hours > 0 && hours < 13? 0: fHate;
+    hours = hours === 0 && fHate? 24: hours;
 
     return {
         'fullDate': `${zeroPen(date.getDate())}/${zeroPen(date.getMonth() + 1)}/${date.getFullYear()}`,
         'longFormDate': `${day}, ${month} ${date.getDate()} ${date.getFullYear()}`,
-        'fullTime': `${zeroPen(hours - fHate)}:${zeroPen(date.getMinutes())}${fHateMark}`
+        'fullTime': `${zeroPen(hours - tempfhate)}:${zeroPen(date.getMinutes())}${fHateMark}`
     }
 }
 
-function updateValueOnTitle (timeS, dateS = null) {
+function updateValueOnTitle (timeS, dateX, dateS = null) {
     let intervalX = 0;
 
     // play sound at the top of an hour
     timeS.search(/:00/g) > 0 && ping && soundOn? ping.play(): void 0;
-    // ping && soundOn? ping.play(): void 0;
 
     if (dateS) {
         document.title = dateS;
         intervalX = 10000;
     }
 
-    setTimeout(() => document.title = timeS, intervalX);
+    setTimeout(() => document.title = `${timeS} - ${dateX}`, intervalX);
 }
 
 function updateValueOnScreen () {
     const tvalue = timeExtract();
     // is this the start of the day? Midnight?
     const tod = tvalue.fullTime.search(/^00:00$|^12:00 PM$/g);
+    tod? console.log(tod): console.log('NOT TOD', tvalue.fullTime);
 
     dateSec.innerText = tvalue.fullDate;
     timeSec.innerText = tvalue.fullTime;
     fullSec.innerText = tvalue.longFormDate;
 
-    updateValueOnTitle(tvalue.fullTime, tod > 0? tvalue.longFormDate: null);
-    // updateValueOnTitle(tvalue.fullTime, tvalue.longFormDate);
+    updateValueOnTitle(tvalue.fullTime, tvalue.fullDate,tod > 0? tvalue.longFormDate: null);
 }
 
 updateValueOnScreen();
